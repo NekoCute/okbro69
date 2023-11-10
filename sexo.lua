7fdc9a451eed41a0e1007217b7f44802030838d4
NachoDachi <82326156+NekoCute@users.noreply.github.com>
2023-11-11 00:40:01 +0700

    Update sexo.lua

--- sexo.lua
+++ sexo.lua
@@ -1,19 +1,4 @@
+getgenv().FixLag1 = false --Enable This Will Reduce Lag But Remove Silent Aim(Pvp,Mob)
+getgenv().FixLag2 = false -- Enable This Will Reduce Lag But Remove Speed Changer
 script_key = ""
-
-getgenv().Mode = "Idle" -- "Idle","Rage", "AI"
-getgenv().AIJumpState = true --Allow Ai To Jump
-getgenv().StopAutoParry = false
-getgenv().PingBased = true
-getgenv().ForceWin = false --100% Win Need  Telekenis, Premium Only
-getgenv().AutoUseSkill = false
-getgenv().BaseVelocity = 15 --Base On The Server, IDK Where To Get it It Should Be 4-6 Or 0 (Old Server)
-getgenv().BasePredictVelocity = 4 -- Should be From 3-3.5 Work Good With 3 If Old Server
-getgenv().VisualizePath = true -- false //
-getgenv().AutoSpamClickDetect = true -- Spam Click If Near Other Player
-getgenv().CloseRangeAttack = false -- Just Move Forward To The Last Character
-getgenv().AutoClickKeyBind = "X" -- Need The Ball To Be Stopped
-getgenv().AutoClickThreshold = 0.2
-getgenv().DistanceBall = 10
-getgenv().NoUi = false
-
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/fd07660d92cb26891e9acfab9f0c6ba4.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b6b36cec473a0dd48eab35b8272b2384.lua"))()
